45e23fe405cf177f60e6ec3489e0a9b303b23c36
Prajay Ghaghda <Prajay@prajays-mini.home>
2017-05-02 19:44:52 +0100

    Fixed scraping issue

--- rhymer.js
+++ rhymer.js
@@ -1,19 +1,20 @@
 var osmosis = require('osmosis')
 var request = require('request')
-var word = 'hello'
-
+var answer = ''
 // function printError(error) {
 // 	console.error(error.message)	}
 
 function findRhyme(y) {
-var url = 'http://rhymebrain.com/talk?function=getRhymes&word=' + y;
-request(url, function (error,response, body) {
-  var results = JSON.parse(body);
-  console.log(results[0].word + ' THE RHYMED WORD');
-  var answer = String(results[0].word)
-});
 
-}
+	var url = 'http://rhymebrain.com/talk?function=getRhymes&word=' + y;
+	request(url, function (error,response, body) {
+	  var results = JSON.parse(body);
+	  console.log(results[0].word + ' THE RHYMED WORD');
+	  answer = String(results[0].word)	  
+
+	 })
+	return answer
+	}
 
 
 
@@ -21,48 +22,51 @@ request(url, function (error,response, body) {
 // console.log(findRhyme('hello'))
 
 
-
+// This is where the program starts. 
 osmosis
 .get('https://genius.com/')
+// only grabbing the most popular song on the list. Find a way to scrape all. 
 .find('.home_charts_row:first')
 .follow('@href')
-.find('.lyrics .referent')
+.find('.song_body-lyrics  p')
 .set('results')
+.log(console.log) // INCREDIBLY USEFUL, PLEASE USE MORE WHEN DEBUGGING. 
 .data(function(song){
 	parser(song.results);
 })
 
-function parseLyrics(x) {
-	var newText = x.split('\n') // split into line
-	console.log(newText[1] + ' ' + newText[2]) // takes 2nd and 3rd lines
-	var firstLine = newText[1];
-	var splitted = newText[1].split(/\s+/) // first line into words 
-	splitted = splitted[splitted.length-1] // last word of line
-	console.log(splitted + ' LAST WORD OF THE LINE') 
-	var rhymedWord = findRhyme(splitted)
-	console.log(rhymedWord + ' THIS IS THE RHYMED WORD')
-	var swappedLine = firstLine.replace('beat', rhymedWord)
-	console.log(swappedLine + ' SWAPPED LINE ')
-	// returns undefined console.log(rhyme(splitted))
-}
+// function parseLyrics(x) {
+// 	var newText = x.split('\n') // split into line
+// 	console.log(newText[1] + ' ' + newText[2]) 		// takes 2nd and 3rd lines
+// 	var firstLine = newText[1];
+// 	var splitted = newText[1].split(/\s+/) // first line into words 
+// 	splitted = splitted[splitted.length-1] // last word of line
+// 	console.log(splitted + ' LAST WORD OF THE LINE') 
+// 	var rhymedWord = findRhyme(splitted)
+// 	console.log(rhymedWord + ' THIS IS THE RHYMED WORD')
+// 	var swappedLine = firstLine.replace('beat', rhymedWord)
+// 	console.log(swappedLine + ' SWAPPED LINE ')
+// 	// returns undefined console.log(rhyme(splitted))
+// }
 
 
 function parser(z) {
 	var newText = z.split('\n') // split into line
-	console.log(newText[1] + ' ' + newText[2]) // takes 2nd and 3rd lines
+	console.log(typeof newText)
 	var firstLine = newText[1];
 	var splitted = newText[1].split(/\s+/) // first line into words 
 	splitted = splitted[splitted.length-1] // last word of line
 	console.log(splitted)
-	var url = 'http://rhymebrain.com/talk?function=getRhymes&word=' + splitted
-	request(url, function (error,response, body) {
-	  var results = JSON.parse(body);
-	  console.log(results[0].word + ' THE RHYMED WORD');
-	  var answer = String(results[0].word)
-	  changed = newText[1].replace(splitted, answer)
-	  console.log(changed)
+	console.log(findRhyme(splitted))
+	// var url = 'http://rhymebrain.com/talk?function=getRhymes&word=' + splitted
+	// request(url, function (error,response, body) {
+	//   var results = JSON.parse(body);
+	//   console.log(results[0].word + ' THE RHYMED WORD');
+	//   var answer = String(results[0].word)
+	//   changed = newText[1].replace(splitted, answer)
+	//   console.log(changed)
 
-	});
+	// });
 
 
 }
@@ -71,9 +75,3 @@ function parser(z) {
 // that it is wrapped in another function and cannot pass the 
 
 
-// home_charts_row home_charts_row--big home_charts_row--big_border
-
-// home_charts_row home_charts_row--big
-
-// home_charts_row
-// Intro]\nTold her how it is\nHendrix\nI promise, I swear, I swear\nYou heard, spit it, yo\n\n[Chorus]\nPercocets, molly, Percocets\nPercocets, molly, Percocets\nRep the set, gotta rep the set\nChase a check, never chase a bitch\nMask on, fuck it, mask off\nMask on, fuck it, mask off\nPercocets, molly, Percocets\nChase a check, never chase a bitch\nDon\'t chase no bitches\n\n[Verse 1]\nTwo cups, toast up with the gang\nFrom food stamps to a whole \'nother domain\nOut the bottom, I’m the livin\' proof (Super)\nAin\'t compromising, half a million on the Coupe\nDrug houses, lookin\' like Peru\nGraduated, I was overdue\nPink molly, I can bare
