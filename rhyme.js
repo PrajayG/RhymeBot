10d01721f27eab212d33e6fb276340f82d6bce76
PrajayG <wooyay@gmail.com>
2017-10-20 08:08:20 +0100

    Added jQuery scraping functionality

--- rhyme.js
+++ rhyme.js
@@ -1,20 +1,20 @@
 var request = require('request')
 var cheerio = require('cheerio')
 
-console.log('Helllo')
+var LyricUrls = []
 
 
-function grabPage() {
-    
-    request('https://www.genius.com', function(err, resp, html) {
-
+function grabLyricUrls(x) {  
+    request('https://www.genius.com', function(err, resp, html) {   
         console.log('starting scrape')
         if (!err){
         const $ = cheerio.load(html);
-        var links = $('a.class_row', this).attr('ng-href');
-        console.log(links)
+        $("a.chart_row").each(function (index){
+            x.push(($(this).attr('href')))   
+        })
         }
     });
 }
 
-grabPage()
+
+var hello = grabLyricUrls(LyricUrls)
