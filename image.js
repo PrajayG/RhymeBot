72187ad0615a461e490b929d6b8e0cd8a6141838
PrajayG <wooyay@gmail.com>
2017-11-20 21:27:52 +0000

    Added load font function

--- image.js
+++ image.js
@@ -1,14 +1,29 @@
 var Jimp = require("jimp");
+// TO DO
+// Find a way to draw text
+// Make a function that can be imported
+// Use the file saving function to output the file
+// Create a tweet 'module' to use in the main file to tweet
+function createImage() {
+var image = new Jimp(1000, 1000, function (err, image) {
 
-var image = new Jimp(256, 256, function (err, image) {
-
-    image.background(0x1d1d1d)
-    
-
-
+    Jimp.loadFont(Jimp.FONT_SANS_8_BLACK).then(function (font) {
+        image.print(font, 10, 10, "!");
+   
+    });
+    writeImage(image)
     // saving the image, callback once image has been written
-    image.write(__dirname, function() {
-        var file = "tester." + image.getExtension();
-        image.write(file)
-    });    
+    
 });
+
+}
+
+function writeImage(image) {
+image.write(__dirname, function() {
+    var file = "test." + image.getExtension();
+    image.write(file)
+    
+}); 
+}
+
+createImage()
