8f9f1de6f7f40c57915eacade0b3ea73d0db1dbc
PrajayG <wooyay@gmail.com>
2017-11-27 20:43:38 +0000

    Added twitter API functionality

--- image.js
+++ image.js
@@ -4,11 +4,15 @@ var Jimp = require("jimp");
 // Make a function that can be imported
 // Use the file saving function to output the file
 // Create a tweet 'module' to use in the main file to tweet
+var Twitter = require("twitter")
+var keys = require("./keys.js")
 
 var exports = module.exports = {}
 
+
+
 exports.createImage = function(text) {
-    var image = new Jimp(350, 200, 0x000000FF, function (err, image) {
+    var image = new Jimp(350, 100, 0x000000FF, function (err, image) {
         // Loading the font 
         Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then(function (font) {
             image.print(font, 50, 20, text, 300);
@@ -19,3 +23,24 @@ exports.createImage = function(text) {
         });
     });
 }
+
+exports.sendTweet = function(name, image) { 
+
+}
+
+var client = new Twitter({
+    consumer_key: keys.CONSUMER_KEY,
+    consumer_secret: keys.CONSUMER_SECRET,
+    access_token_key: keys.ACCESS_TOKEN,
+    access_token_secret: keys.ACCESS_TOKEN_SECRET
+});
+
+function tweetThis(x) {
+    client.post('statuses/update', {status: x}, function(error, tweet, response) {
+        if (!error) {
+          console.log(tweet.text);
+        }
+      });
+}
+
+tweetThis('Whatever will we do next')
--- rhyme.js
+++ rhyme.js
@@ -11,101 +11,97 @@ var artistTitle = "";
 // 
 
 // Initial Request to Genius for newest songs
-request(
-  {
-    url: "https://www.genius.com"
-  },
-  function(error, response, body) {
-    cheerioParser(body);
-  }
+request({
+        url: "https://www.genius.com"
+    },
+    function (error, response, body) {
+        cheerioParser(body);
+    }
 );
 
 // Parsing Genius HTML for newest song lyric URLs
 function cheerioParser(x) {
-  const $ = cheerio.load(x);
-  var LyricUrls = [];
-  $("a.chart_row").each(function(index) {
-    LyricUrls.push($(this).attr("href"));
-  });
-  var LyricPage = LyricUrls[Math.floor(Math.random() * LyricUrls.length)];
-  scrapeLyrics(LyricPage);
+    const $ = cheerio.load(x);
+    var LyricUrls = [];
+    $("a.chart_row").each(function (index) {
+        LyricUrls.push($(this).attr("href"));
+    });
+    var LyricPage = LyricUrls[Math.floor(Math.random() * LyricUrls.length)];
+    scrapeLyrics(LyricPage);
 }
 
 // Grabbing the lyrics from the randomly selected page
 function scrapeLyrics(page) {
-  console.log(page);
-  request(
-    {
-      url: page
-    },
-    function(error, response, body) {
-      const $ = cheerio.load(body);
-      var songLyrics = $(".lyrics p").text();
-      parseLyrics(songLyrics);
-    }
-  );
+    console.log(page);
+    request({
+            url: page
+        },
+        function (error, response, body) {
+            const $ = cheerio.load(body);
+            var songLyrics = $(".lyrics p").text();
+            parseLyrics(songLyrics);
+        }
+    );
 }
 
 // Finding two lines in the same song and removing spaces
 function parseLyrics(song) {
-  lines = song.split("\n");
-  // long way of remove all verse, chorus elements
-  lines.forEach(function(element) {
-    if (element.startsWith("[")) {
-      var index = lines.indexOf(element);
-      if (index > -1) {
-        lines.splice(index, 1);
-      }
+    lines = song.split("\n");
+    // long way of remove all verse, chorus elements
+    lines.forEach(function (element) {
+        if (element.startsWith("[")) {
+            var index = lines.indexOf(element);
+            if (index > -1) {
+                lines.splice(index, 1);
+            }
+        }
+    });
+    // removing blank spaces
+    lines = lines.filter(function (n) {
+        return n != "";
+    });
+
+    // replacing brackets for adlibs
+    for (i = 0; i < lines.length; i++) {
+        lines[i] = lines[i].replace("(", "");
+        lines[i] = lines[i].replace(")", "");
     }
-  });
-  // removing blank spaces
-  lines = lines.filter(function(n) {
-    return n != "";
-  });
 
-  // replacing brackets for adlibs
-  for (i = 0; i < lines.length; i++) {
-    lines[i] = lines[i].replace("(", "");
-    lines[i] = lines[i].replace(")", "");
-  }
-
-  var randomNo = Math.floor(Math.random() * (lines.length - 1));
-  var bars = [lines[randomNo], lines[randomNo + 1]];
-  console.log(bars);
-  constructRhyme(bars);
+    var randomNo = Math.floor(Math.random() * (lines.length - 1));
+    var bars = [lines[randomNo], lines[randomNo + 1]];
+    console.log(bars);
+    constructRhyme(bars);
 }
 
 
 function constructRhyme(bars) {
-  console.log(bars);
-  var bar = bars[Math.floor(Math.random() * bars.length)];
-  
-  var rhymingWord = bar.split(" ").splice(-1);
-  console.log(rhymingWord);
-  request(
-    {
-      url: "https://api.datamuse.com/words?rel_rhy=" + rhymingWord
-    },
-    function(error, response, body) {
-      try {
-        var parsedObject = JSON.parse(body);
-        for (var i = 1; i <= 30; i++) {
-            if (parsedObject[i].numSyllables > 1) {
-                images.createImage(
-                    bars.join(', ').replace(rhymingWord, parsedObject[0].word)
-                );
-                break
-            }
-            else {
-                console.log('Number of syllables too low')
-            }
-                          
-        }
+    console.log(bars);
+    var bar = bars[Math.floor(Math.random() * bars.length)];
 
-        console.log(bars.toString().replace(rhymingWord, parsedObject[0].word));
-      } catch (error) {
-        console.log("Couldn't find a rhyming word to match" + error);
-      }
-    }
-  );
+    var rhymingWord = bar.split(" ").splice(-1);
+    console.log(rhymingWord);
+
+    request({
+            url: "https://api.datamuse.com/words?rel_rhy=" + rhymingWord
+        },
+        function (error, response, body) {
+            try {
+                var parsedObject = JSON.parse(body);
+                for (var i = 1; i <= 30; i++) {
+                    if (parsedObject[i].numSyllables > 1) {
+                        images.createImage(
+                            bars.join(', ').replace(rhymingWord, parsedObject[0].word)
+                        );
+                        break
+                    } else {
+                        console.log('Number of syllables too low')
+                    }
+
+                }
+                console.log(bars.join(', ').replace(rhymingWord, parsedObject[0].word));
+            } catch (error) {
+                console.log("Couldn't find a rhyming word to match" + error);
+            }
+        }
+    );
 }
